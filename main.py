a9b7050e45baf550481f4b75966ef8745e6cfd8b
1tsM35ome0ne <tim.skype303@gmail.com>
2026-04-23 11:00:40 +1000

    Test case 1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from game import Game
 from menu import Menu
 
+'''Test: This is the german coast guard, what are you thinking about'''
+
 def main():
     menu = Menu()
     game_state = menu.run()  # Call the run method of the Menu class
